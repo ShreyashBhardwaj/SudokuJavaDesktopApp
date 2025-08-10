9f761c0bef5ca2d7c84661be241f1d7e84815e48
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-10 20:15:55 +0530

    Coordinates.java done for now

--- src/main/java/com/example/sudokudesktopapp/problemdomain/Cooridnates.java
+++ src/main/java/com/example/sudokudesktopapp/problemdomain/Cooridnates.java
@@ -1,4 +1,35 @@
 package com.example.sudokudesktopapp.problemdomain;
 
+import java.util.Objects;
+
 public class Cooridnates {
+    private final int x,y;
+
+    public Cooridnates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Cooridnates that = (Cooridnates) o;
+        return x==that.x && y==that.y;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(x,y);
+    }
 }

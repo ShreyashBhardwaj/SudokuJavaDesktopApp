634f4d170d6251877992a1c20ca3b8a85d4bc883
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-10 20:45:26 +0530

    IStorage.java interface almost done

---
+++ src/main/java/com/example/sudokudesktopapp/problemdomain/IStorage.java
@@ -0,0 +1,8 @@
+package com.example.sudokudesktopapp.problemdomain;
+
+import java.io.IOException;
+
+public interface IStorage {
+    void updateGameData(SudokuGame game) throws IOException;
+    SudokuGame getGameData() throws IOException
+}
